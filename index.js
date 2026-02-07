e9636761ad6955cdbfcfb6963c8e13448c64d855
jasemorris772014-sudo <jase.morris772014@gmail.com>
2026-02-07 09:39:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RoadToGlory from './RoadToGlory';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <RoadToGlory />
