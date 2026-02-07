81bbf0060144b53a4e929305f912a308e59ad98e
jasemorris772014-sudo <jase.morris772014@gmail.com>
2026-02-07 12:19:37 -0600

    Update RoadToGlory.js

--- src/RoadToGlory.js
+++ src/RoadToGlory.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect ,useRef } from 'react';
 
 export default function RoadToGlory() {
   const [gameState, setGameState] = useState('intro');
